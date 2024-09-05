428cdfc56a021dc9c3c1ecd12e3740a636f15bf2
YunGeun Jang <ygjang9120@gmail.com>
2024-09-05 11:14:21 +0900

    vercel file delete

--- dartGame/JS/start.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const title = document.querySelector('.title');
-    const bullet = document.querySelector('.bullet');
-        
-    bullet.addEventListener('animationstart', () => {
-        gunshotSound.play();
-    });
-
-});
